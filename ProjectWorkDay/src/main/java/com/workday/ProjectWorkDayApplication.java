94a1628fe327265cc845cfc1478b1617aba16b22
erickamontero <monteroh.ericka@gmail.com>
2019-10-03 20:58:55 -0500

    first commit project workday

---
+++ ProjectWorkDay/src/main/java/com/workday/ProjectWorkDayApplication.java
@@ -0,0 +1,13 @@
+package com.workday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWorkDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWorkDayApplication.class, args);
+	}
+
+}
