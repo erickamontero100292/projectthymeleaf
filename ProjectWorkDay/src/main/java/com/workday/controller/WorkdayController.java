c9f4e9c33ca2521c9b2c6323493a794c9cb2c012
Ericka Montero <monteroh.ericka@gmail.com>
2019-10-07 16:14:20 -0500

    adjustment for the list form of workdays

--- ProjectWorkDay/src/main/java/com/workday/controller/WorkdayController.java
+++ ProjectWorkDay/src/main/java/com/workday/controller/WorkdayController.java
@@ -1,5 +1,7 @@
 package com.workday.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,23 +13,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.workday.model.Workday;
 import com.workday.services.WorkDayService;
+
 @Controller
 @RequestMapping("/create/workday")
 public class WorkdayController {
-	
+
 	@Autowired
 	private WorkDayService workdayService;
-	
+
 	@GetMapping("/new")
 	public String nerWorkDay(Model model) {
-		model.addAttribute("work",new Workday());
+		model.addAttribute("work", new Workday());
 		return "create/form-workday";
 	}
- 
+
 	@PostMapping("/new/submit")
-	public String submitNewWorkDay(@ModelAttribute("workday")Workday workday, Model model) {
-		
+	public String submitNewWorkDay(@ModelAttribute("workday") Workday workday, Model model) {
+
 		workdayService.save(workday);
-		return "index";
+		List<Workday> workDays = new ArrayList<Workday>(workdayService.findAll());
+		model.addAttribute("workdays",workDays);
+		
+		return "list/list-workday";
 	}
+
+	@GetMapping("/list")
+	public String listWorkDay(Model model) {
+		List<Workday> workDays = new ArrayList<Workday>(workdayService.findAll());
+		model.addAttribute("workdays",workDays);
+		return "list/list-workday";
+	}
+
 }
