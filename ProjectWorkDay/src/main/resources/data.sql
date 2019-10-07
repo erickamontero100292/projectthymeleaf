f81a1faec4a736990c48b310514f317521d92dd9
Ericka Montero <ezmonte@transunion.com>
2019-10-07 09:27:47 -0500

    adjust in the name table

--- ProjectWorkDay/src/main/resources/data.sql
+++ ProjectWorkDay/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 
-INSERT INTO workday (id,name, numberHour) VALUES (1,'Jornada laboral',8);
-INSERT INTO workday (id,name, numberHour) VALUES (2,'Jornada laboral',12);
+INSERT INTO workday (id,name, number_hour) VALUES (1,'Jornada laboral',8);
+INSERT INTO workday (id,name, number_hour) VALUES (2,'Jornada laboral',12);
 
 
 
