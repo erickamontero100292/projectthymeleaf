ee726470c754e2de382e3c6358deb374b7f19fa4
Ericka Montero <monteroh.ericka@gmail.com>
2019-10-08 12:21:36 -0500

    adjust for functionality list, edit and delete for employee

--- ProjectWorkDay/src/main/java/com/workday/controller/EmployeeController.java
+++ ProjectWorkDay/src/main/java/com/workday/controller/EmployeeController.java
@@ -1,5 +1,8 @@
 package com.workday.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +10,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.workday.model.Employee;
+import com.workday.model.Workday;
 import com.workday.services.EmployeeService;
 import com.workday.services.WorkDayService;
 
@@ -22,34 +27,75 @@ public class EmployeeController {
 	private WorkDayService workdayService;
 	@Autowired
 	private EmployeeService employeeService;
-	
-	
+
 	@GetMapping("/")
 	public String index(Model model) {
-		
-		model.addAttribute("employee", new Employee());
-		return "create/form-employee";
+		List<Employee> employees = new ArrayList<Employee>(employeeService.findAll());
+
+		model.addAttribute("employees", employees);
+		return "list/list-employee";
 	}
 	
+	@GetMapping("/list")
+	public String listEmployee(Model model) {
+		List<Employee> employees = new ArrayList<Employee>(employeeService.findAll());
+		model.addAttribute("employees",employees);
+		return "list/list-employee";
+	}
+	
+
 	@GetMapping("/new")
 	public String newemployee(Model model) {
-		
+
 		model.addAttribute("employee", new Employee());
-		model.addAttribute("workdays",workdayService.findAll());
+		model.addAttribute("workdays", workdayService.findAll());
 		return "create/form-employee";
 	}
-	
+
 	@PostMapping("/new/submit")
-	public String newEmployee(@Valid Employee employee,BindingResult bindingResult, Model model ) {
-		String url="index";
-		if(bindingResult.hasErrors()) {
+	public String newEmployee(@Valid Employee employee, BindingResult bindingResult, Model model) {
+		String url = "list/list-employee";
+		if (bindingResult.hasErrors()) {
 			model.addAttribute("workdays", workdayService.findAll());
-			url= "create/form-employee";
-		}else {
+			url = "create/form-employee";
+		} else {
 			employeeService.save(employee);
-			
+			List<Employee> employees = new ArrayList<Employee>(employeeService.findAll());
+			model.addAttribute("employees",employees);
+
 		}
-		
+
 		return url;
 	}
+
+	@GetMapping("/edit/{id}")
+	public String editEmployee(@PathVariable("id") Long id, Model model) {
+
+		String url = "";
+		Employee employee = employeeService.findById(id);
+
+		if (employee != null) {
+			model.addAttribute("employee", employee);
+			model.addAttribute("workdays", workdayService.findAll());
+			url = "create/form-employee";
+
+		} else {
+			url = "redirect:/create/employee/";
+		}
+		return url;
+	}
+	@GetMapping("/delete/{id}")
+	public String deleteEmployee(@PathVariable ("id") Long id, Model model) {
+		
+		String url ="";
+		Employee employee= employeeService.findById(id);
+		
+		
+		if(employee != null) {
+			employeeService.delete(id);
+			url="redirect:/create/employee/";
+		}
+		return url;
+	}
+
 }
--- ProjectWorkDay/src/main/java/com/workday/services/EmployeeService.java
+++ ProjectWorkDay/src/main/java/com/workday/services/EmployeeService.java
@@ -1,8 +1,11 @@
 package com.workday.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.workday.model.Employee;
+import com.workday.model.Workday;
 import com.workday.repository.EmployeeRepository;
 
 @Service
@@ -17,13 +20,27 @@ public class EmployeeService {
 		this.employeeRepository = employeeRepository;
 	}
 
-
-
-
 	public Employee save (Employee employee){
 		
 		return employeeRepository.save(employee);
 	}
 	
+	
+	public List<Employee>findAll(){
+		return employeeRepository.findAll();
+		
+	}
+	
+	public Employee findById(Long id) {
+		
+		return employeeRepository.findById(id).orElse(null);
+	}
+	
+	
+	public Employee delete(Long id) {
+		Employee employee = findById(id);
+		employeeRepository.delete(employee);
+		return employee;
+	}
 	
 }
